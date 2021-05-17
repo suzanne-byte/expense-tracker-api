dda16e3620a5b3c759e959153db6418ca87e16c3
suzanne-byte <senibalo.suzannebit2011@gmail.com>
2021-05-17 23:33:43 +0800

    Added the first test & github actions.

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",
-    "unittest",
-    "restapi",
+    # "rest_framework",
+    # "unittest",
+    # "restapi",
 ]
 
 MIDDLEWARE = [
--- restapi/tests.py
+++ restapi/tests.py
@@ -1,5 +1,5 @@
-from django.test import TestCase
-from restapi import models
+# from django.test import TestCase
+# from restapi import models
 from unittest import TestCase
 
 # Create your tests here.
@@ -10,3 +10,19 @@ def two_integers_sum(a, b):
 class TestSum(TestCase):
     def test_sum(self):
         self.assertEqual(two_integers_sum(1, 2), 3)
+
+
+# class TestModels(TestCase):
+# def test_expense(self):
+# expense = models.Expense.objects.create(
+# amount=249.99,
+# merchant="amazon",
+# description="anc headphones",
+# category="music",
+# )
+# inserted_expense = models.Expense.objects.get(pk=expense.id)
+
+# self.assertEqual(249.99, inserted_expense.amount)
+# self.assertEqual("amazon", inserted_expense.merchant)
+# self.assertEqual("anc headphones", inserted_expense.description)
+# self.assertEqual("music", inserted_expense.category)
