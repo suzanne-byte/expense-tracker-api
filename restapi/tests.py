37cf52cd47a028c868120b572834e9fbccd0d7bb
suzanne-byte <senibalo.suzannebit2011@gmail.com>
2021-05-17 20:48:01 +0800

    Added the first test & github actions.

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from restapi import models
 
 # Create your tests here.
+class TestModels(TestCase):
+    def test_expense(self):
+        expense = models.Expense.objects.create(
+            amount=249.99,
+            merchant="amazon",
+            description="anc headphones",
+            category="music",
+        )
+        inserted_expense = models.Expense.objects.get(pk=expense.id)
+
+        self.assertEqual(249.99, inserted_expense.amount)
+        self.assertEqual("amazon", inserted_expense.merchant)
+        self.assertEqual("anc headphones", inserted_expense.description)
+        self.assertEqual("music", inserted_expense.category)
