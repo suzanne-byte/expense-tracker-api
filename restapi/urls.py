4b9a4e47ec9c3a3567f4428ef3d2fcb4b035e25d
suzanne-byte <senibalo.suzannebit2011@gmail.com>
2021-05-18 13:00:49 +0800

    Refractored behavior for generic views.

--- restapi/urls.py
+++ restapi/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from restapi import views
 
 urlpatterns = [
-    path("expenses/", views.ExpenselistCreate.as_view(), name="expense-list-create")
+    path("expenses/", views.ExpenselistCreate.as_view(), name="expense-list-create"),
+    path(
+        "expenses/<pk>",
+        views.ExpenseRetriveDelete.as_view(),
+        name="expense-retrieve-delete",
+    ),
 ]
--- restapi/views.py
+++ restapi/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from restapi import models, serializers
 from django.forms.models import model_to_dict
+from rest_framework.generics import RetrieveDestroyAPIView, ListCreateAPIView
 
 # Create your views here.
-class ExpenselistCreate(APIView):
-    def get(self, request):
-        expenses = models.Expense.objects.all()
-        serializer = serializers.Expense(expenses, many=True)
-        return Response(serializer.data, status=200)
+class ExpenselistCreate(ListCreateAPIView):
+    serializer_class = serializers.Expense
+    queryset = models.Expense.objects.all()
 
-    def post(self, request):
-        serializer = serializers.Expense(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
 
-        return Response(serializer.data, status=201)
+class ExpenseRetriveDelete(RetrieveDestroyAPIView):
+    serializer_class = serializers.Expense
+    queryset = models.Expense.objects.all()
