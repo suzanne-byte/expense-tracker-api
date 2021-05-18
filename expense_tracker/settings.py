9711a5cfb5b2349479c82a3d1cacf81a846846e3
suzanne-byte <senibalo.suzannebit2011@gmail.com>
2021-05-19 00:09:49 +0800

    Added API key to the views and added it to the request headers of the client used to makes tests requests.

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -121,8 +121,7 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework_api_key.permissions.HasAPIKey"],
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"]
 }
 
 TESTING = "test" in sys.argv[1:]
